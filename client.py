4744f6aaea451bbe24f6f490c950fcca46dbf58d
Ates Goral <ates.goral@shopify.com>
2020-03-15 14:58:23 -0400

    Add new pins

--- client.py
+++ client.py
@@ -4,13 +4,25 @@ pinR = 17
 pinG = 16
 pinB = 15
 
+pinLatch = 18
+pinClock = 19
+pinData = 20
+
 gpioR = onionGpio.OnionGpio(pinR)
 gpioG = onionGpio.OnionGpio(pinG)
 gpioB = onionGpio.OnionGpio(pinB)
 
+gpioLatch = onionGpio.OnionGpio(pinLatch)
+gpioClock = onionGpio.OnionGpio(pinClock)
+gpioData = onionGpio.OnionGpio(pinData)
+
 gpioR.setOutputDirection()
 gpioG.setOutputDirection()
 gpioB.setOutputDirection()
+
+gpioLatch.setOutputDirection()
+gpioClock.setOutputDirection()
+gpioData.setOutputDirection()
 
 gpioR.setValue(0)
 gpioG.setValue(1)
