6dad49e40d7786701b312aa99e8ef13ea1325c76
Ates Goral <ates.goral@shopify.com>
2020-03-15 16:36:47 -0400

    Add reset script

---
+++ reset.sh
@@ -0,0 +1,7 @@
+echo 17 > /sys/class/gpio/unexport
+echo 16 > /sys/class/gpio/unexport
+echo 15 > /sys/class/gpio/unexport
+
+echo 18 > /sys/class/gpio/unexport
+echo 19 > /sys/class/gpio/unexport
+echo 20 > /sys/class/gpio/unexport
